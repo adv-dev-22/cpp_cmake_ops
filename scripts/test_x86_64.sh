ecfeaee1e9077482d1819306cdd52f71f292677f
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2024-01-05 17:34:31 +0300

    Test script x86_64 added

--- scripts/test_x86_64.sh
+++ scripts/test_x86_64.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+echo "------------------ Test x86_64 Started ------------------"
 
+SRC_PATH=$WORKSPACE
+docker run --rm --name img_run_x86_64_12 -v $SRC_PATH:/app/src ubuntu_x86_84:v12 \
+    "cd /app/src/build-x86_64 && cmake --build . --target test > results_test_$BUILD_NUMBER.txt"
+less $SRC_PATH/build-x86_64/results_test_$BUILD_NUMBER.txt
 
+echo "------------------ Test x86_64 Finished ------------------"
