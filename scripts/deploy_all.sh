9bba7f23ef19e28f28fdf7c97776e74378b353a2
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2024-01-05 18:07:49 +0300

    Deploy script added

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "------------------ Deploy Started ------------------"
 
+SRC_PATH=$WORKSPACE
+scp -o StrictHostKeyChecking=no $SRC_PATH/build-x86_64/libtiff/libtiff.so \
+    $DEPLOY_USER_VM2@$IP_UBUNTU_DEPLOY:/home/$DEPLOY_USER_VM2/deploy/build-x86_64/libs_out/libtiff_$BUILD_NUMBER.so
+scp -o StrictHostKeyChecking=no $SRC_PATH/build-arm32v7/libtiff/libtiff.so \
+    $DEPLOY_USER_VM2@$IP_UBUNTU_DEPLOY:/home/$DEPLOY_USER_VM2/deploy/build-arm32v7/libs_out/libtiff_$BUILD_NUMBER.so
 
+echo "------------------ Deploy Finished ------------------"
