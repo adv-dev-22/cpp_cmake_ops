6cf6f2fc4a68bc6b94caf3639fe8d8e91b96d282
adv-dev-22 <a2nced.c0mputin9.lms.17551@yandex.ru>
2024-01-05 17:56:03 +0300

    Build script arm32v7 added

--- scripts/build_arm32v7.sh
+++ scripts/build_arm32v7.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo "------------------ Build arm32v7 Started ------------------"
+
+SRC_PATH=$WORKSPACE
+mkdir build-arm32v7
+docker run --rm --name img_run_arm32v7_01 -v $SRC_PATH:/app/src \
+    -v $REPO_UTILS_PATH/cpp_cmake_ops/cmake-configs:/cmake-toolchain ubuntu_arm32v7:v01 \
+    "cd /app/src/build-arm32v7 && \
+     cmake -DCMAKE_TOOLCHAIN_FILE=/cmake-toolchain/toolchain_arm32v7.cmake .. && cmake --build ."
+ls build-arm32v7/libtiff
+
+echo "------------------ Build arm32v7 Finished ------------------"
